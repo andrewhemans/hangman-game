bac1560964874b16810be792770cdcdeea197b0f
andrewhemans <andrewhemans@gmail.com>
2018-03-12 23:33:11 -0700

    so much stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,83 +2,251 @@ console.log("it's alive!")
 
 // Create a library of names for characters in Star Trek
 
-  var characters = ['Kirk', 'Spock', 'Picard', 'Riker', 'Kira', 'Dax', 'Sisqo', 'Janeway', 'Chakotay', 'Data', 'LaForge', 'Surak', 'Kahn', 'Archer', 'Gowron', 'Worf'];
+  var characters = ['Kirk', 'Spock', 'Picard', 'Riker', 'Kira', 'Jadzia', 'Sisko', 'Janeway', 'Chakotay', 'Data', 'LaForge', 'Sarek', 'Khan', 'Archer', 'Martok', 'Worf'];
 
-  // Creating variables to hold the number of wins, losses, and ties. They start at 0.
-  var wins = 0;
-  var losses = [];
+  var wrong = [];
 
+// click start game to begin game
 
-// When user presses "start game" coputer chooses random name from library.
-// computer chooses a name
-var computerGuess = characters[Math.floor(Math.random() * characters.length)];
+function changeNav() {
+  var parent = document.getElementById("nav-list");
+  var child = document.getElementById("start-game");
+  parent.removeChild(child);
 
-computerGuess = computerGuess.toLowerCase();
+  var newItem = document.createElement("li");
+  var newLine = document.createTextNode("play again");
+  newItem.appendChild(newLine);
 
-// convert computerGuess to an array
-var nameArr = computerGuess.split('');
+  var newNav = document.getElementById("nav-list");
+  newNav.insertBefore(newItem, newNav.childNodes[2]);
 
-// count number of letters in name
-charInName = [];
-
-// Display blank spaces with underscores for each letter in name.
-
-for (var i = 0; i < nameArr.length; i++) {
-  charInName.push("_");
+  newItem.setAttribute("onclick", "playAgain()");
+  newItem.setAttribute("id", "play-now");
 }
 
+function logWrong() {
+  // logs wrong guesses
+  var wrongGuess =
+    "<p>" + wrong.join(', ') + "</p>";
+
+  document.querySelector("#wrong-guess").innerHTML = wrongGuess;
+}
+
+function resetScores() {
+    location.reload();
+}
+
+function startGame() {
+
+  changeNav();
+
+  // resets everything except the score
+  document.getElementById("play-now").onclick = function() {playAgain()};
+
+  document.getElementById("give-up").onclick = function() {giveUp()};
+
+  var wins = 0;
+
+  var losses = 0;
+
+    // When user presses "start game" coputer chooses random name from library.
+    // computer chooses a name
+    var computerGuess = characters[Math.floor(Math.random() * characters.length)];
+
+    computerGuess = computerGuess.toLowerCase();
+
+    // convert computerGuess to an array
+    var nameArr = computerGuess.split('');
+
+    // count number of letters in name
+    charInName = [];
+
+    // Display blank spaces with underscores for each letter in name.
+
+    for (var i = 0; i < nameArr.length; i++) {
+      charInName.push("_");
+    }
+
+    // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+    var html =
+      "<p>" + charInName.join(" ") + "</p>";
+
+      document.querySelector("#game").innerHTML = html;
 
 
+    // hide intro text
+    document.getElementById("begin").style.display = "none";
+    document.getElementById("main-game").style.display = "block";
 
-// when a user presses a key check to see if that letter is in the names
+    // testing adding new element
+    function showImage() {
+      var targetDiv = document.getElementById("blank");
+      var newDiv = document.createElement("div");
+      // newDiv.textContent = "A pleasure to meet you!";
+
+      var image = document.createElement("IMG");
+      image.setAttribute("src", "assets/images/" + computerGuess + ".png");
+      image.setAttribute("id", "char-image");
+
+      targetDiv.appendChild(image);
+      targetDiv.appendChild(newDiv);
+      newDiv.setAttribute("class", "image");
+      }
+
+    //clear name and generate a new onkeyup
+    function clearName() {
+      // clears name
+      var parent = document.getElementById("game");
+      var child = document.getElementById("name");
+      parent.removeChild(child);
+
+      // computer chooses a name
+      computerGuess = characters[Math.floor(Math.random() * characters.length)];
+
+      computerGuess = computerGuess.toLowerCase();
+
+      // convert computerGuess to an array
+      nameArr = computerGuess.split('');
+
+      // count number of letters in name
+      charInName = [];
+
+      // Display blank spaces with underscores for each letter in name.
+
+      for (var i = 0; i < nameArr.length; i++) {
+        charInName.push("_");
+      }
+
+      // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+      html =
+        "<p>" + charInName.join(" ") + "</p>";
+
+        document.querySelector("#game").innerHTML = html;
+
+    }
+
+    function playAgain() {
+      // clears image
+      var parent = document.getElementById("blank");
+      var child = document.getElementById("char-image");
+      parent.removeChild(child);
+
+      // clears wrong guesses
+      wrong.length = 0;
+      clearName();
+      logWrong();
+    }
+
+    function scoreWin() {
+      if (wins === 1) {
+        // logs the wins
+        var winsText = document.createElement("P");
+        var winsNumber = document.createTextNode(wins);
+          winsText.appendChild(winsNumber);
+          winsText.setAttribute("class", "score-number");
+          winsText.setAttribute("id", "wins-number");
+
+        document.getElementById("score").appendChild(winsText);
+
+        var winsLabel = document.createElement("P");
+        var winsLabelText = document.createTextNode("wins");
+          winsLabel.appendChild(winsLabelText);
+          winsLabel.setAttribute("class", "score-text");
+
+        document.getElementById("score").appendChild(winsLabel);
+      }
+      else if (wins >= 2) {
+        document.getElementById("wins-number").innerHTML = wins;
+      }
+    }
+
+    function scoreLoss() {
+      if (losses === 1) {
+        // logs the losses
+        var lossText = document.createElement("P");
+        var lossNumber = document.createTextNode(losses);
+          lossText.appendChild(lossNumber);
+          lossText.setAttribute("class", "score-number");
+          lossText.setAttribute("id", "loss-number");
+
+        document.getElementById("score").appendChild(lossText);
+
+        var lossLabel = document.createElement("P");
+        var lossLabelText = document.createTextNode("Losses");
+          lossLabel.appendChild(lossLabelText);
+          lossLabel.setAttribute("class", "score-text");
+
+        document.getElementById("score").appendChild(lossLabel);
+      }
+      else if (losses >= 2) {
+        document.getElementById("loss-number").innerHTML = losses;
+      }
+    }
+
+    function giveUp() {
+        showImage();
+        losses++;
+        scoreLoss();
+      }
+
+
+    // when a user presses a key check to see if that letter is in the names
     // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
+    document.onkeyup = function keyPress(event) {
 
       // Determines which key was pressed.
       var userGuess = event.key;
 
+      // for names with multiple instances of same letter
+      var str = computerGuess;
+      var indices = [];
+      for(var j=0; j<str.length;j++) {
+          if (str[j] === userGuess) indices.push(j);
+      }
+      // console.log(indices);
+
         // run this code if a key input matches
         if (computerGuess.indexOf(userGuess) >= 0) {
-          wins++;
-
           // if a letter matches replace that blank space with the letters
-          charInName[computerGuess.indexOf(userGuess)] = userGuess;
-          console.log(charInName);
+          if (indices.length === 1) {
+            charInName[indices[0]] = userGuess;
+          }
+          else if (indices.length === 2) {
+            charInName[indices[0]] = userGuess;
+            charInName[indices[1]] = userGuess;
+          }
+
         }
+
 
         else if (computerGuess.indexOf(userGuess) === -1) {
 
           // if guessed letter does not match any letter in the name then logged that letter as a failed guessed
-          losses.push(userGuess);
-          console.log(losses);
-        } else {
-
+          wrong.push(userGuess);
+          console.log(wrong);
         }
 
         // if user guesses the name in less than 13 guesses show winning message and display image of characters
         if (charInName.join('') === computerGuess) {
-          console.log("you Win!");
+          wins++;
+          showImage();
+          scoreWin();
         }
 
         // user can have 13 wrong guesses before they lose the game.
-        else if (losses.length === 9) {
+        else if (wrong.length === 9) {
           console.log('you lose');
-
+          showImage();
+          losses++;
+          scoreLoss();
         }
 
+        logWrong();
+          // Set the inner HTML contents of the #game div to our html string
+          var html =
+            "<p id='name'>" + charInName.join(" ") + "</p>";
 
-        // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
-        var html =
-          "<p>You chose: " + userGuess + "</p>" +
-          "<p>The computer chose: " + computerGuess + "</p>" +
-          "<p>" + charInName.join(" ") + "</p>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses.join(', ') + "</p>";
-
-        // Set the inner HTML contents of the #game div to our html string
-        document.querySelector("#game").innerHTML = html;
+          document.querySelector("#game").innerHTML = html;
 
       };
-
-
-// if user loses game then show losing message, image of character and show the name.
+    }
